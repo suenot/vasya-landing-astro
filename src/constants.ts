4c2b011f28d2371e971638760e9c1b19c550cbd9
suenot <suenot@gmail.com>
2026-02-28 10:21:56 +0300

    refactor: Extract constants and consolidate layout components

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const APP_VERSION = 'v0.1.2';
+export const RELEASE_URL = 'https://github.com/suenot/vasya/releases/download/v0.1.2';
+export const GITHUB_URL = 'https://github.com/suenot/vasya';
